653739a8a37288ba2dc7b2b000f60d79e2b30688
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-09-11 03:15:38 -0500

    Remove warnings from test_app.R

--- tests/testthat/test-test-app.R
+++ tests/testthat/test-test-app.R
@@ -5,7 +5,5 @@ test_that("analyzeBehaviorspace works", {
   skip_on_cran()
 
   appdir <- system.file(package="analyzeBehaviorspace", "analyzeBehaviorspace")
-  warning("appdir = ", appdir)
-  warning("appdir contains [", str_c(list.files(appdir), collapse = ", "), "]")
   expect_pass(testApp(appdir, compareImages = FALSE))
 })
