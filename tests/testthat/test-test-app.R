e49ca60815bf6249b170ea9e9a797252c2c4dba0
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-09-11 02:40:22 -0500

    Updated testthat material.

--- tests/testthat/test-test-app.R
+++ tests/testthat/test-test-app.R
@@ -1,8 +1,11 @@
 context("app-file")
 library(shinytest)
+library(stringr)
 test_that("analyzeBehaviorspace works", {
   skip_on_cran()
 
   appdir <- system.file(package="analyzeBehaviorspace", "analyzeBehaviorspace")
+  warning("appdir = ", appdir)
+  warning("appdir contains [", str_c(list.files(appdir), collapse = ", "), "]")
   expect_pass(testApp(appdir, compareImages = FALSE))
 })
