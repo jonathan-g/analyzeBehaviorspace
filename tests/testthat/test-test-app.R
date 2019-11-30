299ef276a226c7b07be36a58e7ff6e508a962af4
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2019-11-30 10:18:16 -0600

    Another try with travis and shinytest.

--- tests/testthat/test-test-app.R
+++ tests/testthat/test-test-app.R
@@ -4,5 +4,6 @@ test_that("analyzeBehaviorspace works", {
   skip_on_cran()
 
   appdir <- system.file("abs_app", package="analyzeBehaviorspace")
+  expect_true(dir.exists(!!appdir))
   expect_pass(testApp(appdir, compareImages = FALSE, interactive = FALSE))
 })
