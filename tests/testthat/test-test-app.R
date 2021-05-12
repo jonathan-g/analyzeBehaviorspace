89784decf01a98ea507f9c7d4f3b8655b9b7b25d
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-05-12 01:12:30 -0500

    Using local file paths for ABS app in testthat does not work with R CMD check, so go back to using system.file().

--- tests/testthat/test-test-app.R
+++ tests/testthat/test-test-app.R
@@ -2,9 +2,11 @@ context("app-file")
 test_that("analyzeBehaviorspace works", {
   skip_on_cran()
 
-  # appdir <- system.file("abs_app", package="analyzeBehaviorspace")
-  appdir <- "inst/abs_app"
+  appdir <- system.file("abs_app", package="analyzeBehaviorspace")
+  # appdir <- "../../inst/abs_app"
+  appdir <- normalizePath(appdir, winslash = "/")
   message("appdir = ", appdir)
+  # expect_equal(appdir, "D:/JG_Documents/programming/Teaching/teaching_projects/analyzeBehaviorspace/inst/abs_app")
   expect_true(dir.exists(appdir))
   if (require(shinytest)) {
     os_type = stringr::str_split(utils::osVersion, " ")[[1]][1]
