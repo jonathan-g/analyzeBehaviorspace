787532c2184d775b82daf75d93e20688e921da3f
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-05-12 01:43:13 -0500

    Better debugging information from shinytests under testthat.

--- tests/testthat/test-test-app.R
+++ tests/testthat/test-test-app.R
@@ -6,8 +6,13 @@ test_that("analyzeBehaviorspace works", {
   # appdir <- "../../inst/abs_app"
   appdir <- normalizePath(appdir, winslash = "/")
   message("appdir = ", appdir)
+  os_type <- stringr::str_split(utils::osVersion, " ")[[1]][1]
   # expect_equal(appdir, "D:/JG_Documents/programming/Teaching/teaching_projects/analyzeBehaviorspace/inst/abs_app")
   expect_true(dir.exists(appdir))
+  expect_true(
+    dir.exists(file.path(appdir, "tests", "shinytest",
+                         stringr::str_c("test_abs-expected-", os_type)))
+    )
   if (require(shinytest)) {
     os_type = stringr::str_split(utils::osVersion, " ")[[1]][1]
     message("OS version = ", os_type)
