a5fe1a6e98be8435042912fe50d77d9df3fed62f
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-05-12 00:51:03 -0500

    Fix testthat script to work with local version of the app rather than system.file() for installed version.

--- tests/testthat/test-test-app.R
+++ tests/testthat/test-test-app.R
@@ -2,8 +2,10 @@ context("app-file")
 test_that("analyzeBehaviorspace works", {
   skip_on_cran()
 
-  appdir <- system.file("abs_app", package="analyzeBehaviorspace")
-  expect_true(dir.exists(!!appdir))
+  # appdir <- system.file("abs_app", package="analyzeBehaviorspace")
+  appdir <- "../../inst/abs_app"
+  message("appdir = ", appdir)
+  expect_true(dir.exists(appdir))
   if (require(shinytest)) {
     os_type = stringr::str_split(utils::osVersion, " ")[[1]][1]
     message("OS version = ", os_type)
