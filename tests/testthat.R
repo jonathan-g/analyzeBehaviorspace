d938686a79fb2ab0f121886753c7b4faa05a74e3
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2021-11-18 00:30:22 -0600

    Debugging shinytest tests.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,11 @@
 library(testthat)
 library(analyzeBehaviorspace)
 
-# appdir <- system.file("abs_app", package="analyzeBehaviorspace")
-# message("appdir = ", appdir, ". Contents = [",
-#         paste(list.files(appdir), collapse = ", "), "]")
+appdir <- system.file("abs_app", package="analyzeBehaviorspace")
+appdir <- normalizePath(appdir, winslash = "/")
+message("appdir = ", appdir)
+
+os_type <- stringr::str_split(utils::osVersion, " ")[[1]][1]
+message("OS version = ", os_type)
 
 test_check("analyzeBehaviorspace", reporter = "summary")
--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -3,11 +3,9 @@ test_that("analyzeBehaviorspace works", {
   skip_on_cran()
 
   appdir <- system.file("abs_app", package="analyzeBehaviorspace")
-  # appdir <- "../../inst/abs_app"
   appdir <- normalizePath(appdir, winslash = "/")
   message("appdir = ", appdir)
   os_type <- stringr::str_split(utils::osVersion, " ")[[1]][1]
-  # expect_equal(appdir, "D:/JG_Documents/programming/Teaching/teaching_projects/analyzeBehaviorspace/inst/abs_app")
   expect_true(dir.exists(appdir))
   expect_true(
     dir.exists(file.path(appdir, "tests", "shinytest",
