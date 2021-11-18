540baa146c3a701918b95dab593b82beb9a793e5
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2021-11-18 00:46:50 -0600

    More debugging shinytest for GitHub actions.

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -6,17 +6,17 @@ test_that("analyzeBehaviorspace works", {
   appdir <- normalizePath(appdir, winslash = "/")
   message("appdir = ", appdir)
   os_type <- stringr::str_split(utils::osVersion, " ")[[1]][1]
-  expect_true(dir.exists(appdir))
+  message("OS version = ", os_type)
+  expect_true(dir.exists(appdir), info = "app dir exists.")
+  resdir <- file.path(appdir, "tests", "shinytest",
+                      stringr::str_c("test_abs-expected-", os_type))
   expect_true(
-    dir.exists(file.path(appdir, "tests", "shinytest",
-                         stringr::str_c("test_abs-expected-", os_type)))
-    )
+    dir.exists(resdir), info = "expected test results directory exists.")
   if (require(shinytest)) {
-    os_type = stringr::str_split(utils::osVersion, " ")[[1]][1]
-    message("OS version = ", os_type)
     expect_pass(
-      testApp(appdir, compareImages = FALSE, interactive = FALSE,
-              suffix = os_type, quiet = TRUE)
+      testApp(appdir, suffix = os_type, quiet = TRUE,
+              compareImages = FALSE, interactive = FALSE),
+      info = "shinytest passes."
     )
   }
 })
