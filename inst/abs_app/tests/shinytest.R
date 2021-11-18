79b7f9fff30996953a4df7c9677db6a54c2d09c3
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2021-11-17 23:55:29 -0600

    Clean up tests.

--- inst/abs_app/tests/shinytest.R
+++ inst/abs_app/tests/shinytest.R
@@ -2,5 +2,5 @@ library(tidyverse)
 library(shinytest)
 os_type = stringr::str_split(utils::osVersion, " ")[[1]][1]
 message("OS version = ", os_type)
-shinytest::testApp("../", suffix = os_type)
+shinytest::testApp("..", suffix = os_type)
 
